1049d51cb5eb77684eaeaf84531642def63e64c6
Satish Kumar Karlapudi <skarlapu@Satishs-MacBook-Pro.local>
2023-01-23 12:01:42 -0500

    Initial Commit

---
+++ src/test/java/com/learner/DockerJenkinsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
